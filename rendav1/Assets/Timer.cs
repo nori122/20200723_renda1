8aad0a38b1f1a2704bdb0fbfbb7e637aafc931d0
nori122 <norito.sasaki.jp@gmail.com>
2020-07-23 15:05:14 +0900

    v1.1

---
+++ rendav1/Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    int displaytimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.GetComponent<Text>().text = Convert.ToString(displaytimer);
+        if (displaytimer < 300)
+        {
+            // タイマーを増やす
+            displaytimer++;
+        }
+
+    }
+
+}
---
+++ rendav1/Assets/Touch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class Touch : MonoBehaviour
+{
+    // カウント用変数
+    int Cnt;
+    // 結果表示用変数
+    int result;
+    // タイマー用変数
+    int timer;
+
+    // フレーム毎に呼ばれる関数
+    void Update()
+    {
+        // マウスがクリックされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            // カウントを増やす
+            Cnt++;
+        }
+
+        //カウントをTextコンポーネントへ 
+        this.GetComponent<Text>().text = Cnt + "クリック";
+
+        // もしtimerが500以上になったら
+        if (timer > 300)
+        {
+            // カウントを0に戻す
+            //カウントをTextコンポーネントへ 
+            this.GetComponent<Text>().text = "結果は" + result + "クリックでした～";
+        }
+        else
+        {
+            result = Cnt;
+        }
+        // タイマーを増やす
+        timer++;
+    }
+}
